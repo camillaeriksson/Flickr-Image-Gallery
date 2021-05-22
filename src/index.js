3ca2bdc37bc3c39c0890875eb99d7726fd91b412
Camilla Eriksson <ericsson.camilla@gmail.com>
2021-05-22 21:35:12 +0200

    Fixed indent

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 function fetchImages() {
 	fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=97a2010d6449784728e75b5b59424c57&tags=nature&format=json&nojsoncallback=1')
-	.then(response => response.json())
-	.then (data => console.log(data))
-	.catch(error => {
-		console.log(error)
-	})
+		.then(response => response.json())
+		.then (data => console.log(data))
+		.catch(error => {
+			console.log(error)
+		})
 }
 
 fetchImages()
