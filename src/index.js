83d3a3674853283997b8406f01168345bb90d4d3
Camilla Eriksson <ericsson.camilla@gmail.com>
2021-05-22 21:34:47 +0200

    Fetching and console logging photos from api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 function fetchImages() {
-	console.log('fetch images here')
+	fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=97a2010d6449784728e75b5b59424c57&tags=nature&format=json&nojsoncallback=1')
+	.then(response => response.json())
+	.then (data => console.log(data))
+	.catch(error => {
+		console.log(error)
+	})
 }
 
 fetchImages()
