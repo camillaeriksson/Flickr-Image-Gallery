6dde56b454ee404726c6c5ec608c6963303d922c
Camilla Eriksson <ericsson.camilla@gmail.com>
2021-05-22 22:16:06 +0200

    Testing the function that will later print the img

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 function fetchImages() {
 	fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=97a2010d6449784728e75b5b59424c57&tags=nature&format=json&nojsoncallback=1')
 		.then(response => response.json())
-		.then (data => console.log(data))
+		.then (data => printImages(data.photos.photo))
 		.catch(error => {
 			console.log(error)
 		})
 }
+
+function printImages() {
+	document.querySelector('#image-container').insertAdjacentHTML('afterbegin', '<h1>Hello World</h1>')
+}
+
 
 fetchImages()
