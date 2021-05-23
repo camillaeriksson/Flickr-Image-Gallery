7720781a28493730b08c1d65535fb190f5064f43
Camilla Eriksson <ericsson.camilla@gmail.com>
2021-05-23 11:43:28 +0200

    Added function do display loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+window.onload = fetchImages()
+
 function fetchImages() {
+	displayLoader(true)
 	fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=97a2010d6449784728e75b5b59424c57&tags=nature&format=json&nojsoncallback=1')
 		.then(response => response.json())
 		.then (data => printImages(data.photos.photo))
+		.then(() => displayLoader(false))
 		.catch(error => {
 			console.log(error)
 		})
@@ -9,10 +13,16 @@ function fetchImages() {
 
 function printImages(data) {
 	data.map((image) => {
-		const imageUrl = 'https://live.staticflickr.com/'+image.server+'/'+image.id+'_'+image.secret+'.jpg'
+		const imageUrl = `https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`
 		document.querySelector('#image-container').insertAdjacentHTML('afterbegin', `<img src="${imageUrl}" alt="Girl in a jacket" width="500" height="600">`)
 	})
 }
 
-
-fetchImages()
+function displayLoader(isLoading) {
+	console.log(isLoading)
+	if (isLoading === true) {
+		document.querySelector('#image-container').innerHTML = '<div id="loader">LOADING</div>'
+	} else if (isLoading === false) {
+		document.querySelector('#loader').remove()
+	}
+}
