6aadf91715b3f8a43e59b525e4471d9e317c0a04
Camilla Eriksson <ericsson.camilla@gmail.com>
2021-05-24 20:09:31 +0200

    Made overlay on hover and styled it.

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ window.addEventListener('scroll', () => {
 function fetchImages() {
 	isLoading = true
 	displayLoader(isLoading)
-	fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&in_gallery=true&api_key=97a2010d6449784728e75b5b59424c57&tags=clouds&sort=interestingness-desc&per_page=18&page=${currentPage}&format=json&nojsoncallback=1`)
+	fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&in_gallery=true&api_key=97a2010d6449784728e75b5b59424c57&tags=clouds&extras=owner_name%2C+views&sort=interestingness-desc&per_page=18&page=${currentPage}&format=json&nojsoncallback=1`)
 		.then(response => response.json())
 		.then (data => printImages(data.photos.photo))
 		.then(() => {
@@ -37,8 +37,22 @@ function fetchImages() {
 
 function printImages(data) {
 	data.map((image) => {
+		let imageTitle = ''
+		if (image.title) {
+			imageTitle = image.title.split(' ').splice(0,3).join(' ')
+		} else {
+			imageTitle = 'Untitled'
+		}
+		console.log(image)
 		const imageUrl = `https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}_w.jpg`
-		const imageElement = `<div class="single-image-container"><img src="${imageUrl}" alt="${image.tite}" /></div>`
+		const imageElement = 
+		`<div class="single-image-container">
+			<img src="${imageUrl}" alt="${image.tite}" />
+			<div class="image-info-overlay">
+				<h4>${imageTitle}</h4>
+				<p>${image.ownername}</p>
+			</div>
+		</div>`
 		document.querySelector('#all-images-container').insertAdjacentHTML('beforeend', imageElement)
 	})
 }
